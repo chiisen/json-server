016b1ca65a5d2ea768fd01d5a75c4d16f07da740
sam <chiisen@hotmail.com>
2022-03-17 11:24:42 +0800

    fix: 調整 log 內容

--- middleware-1.js
+++ middleware-1.js
@@ -4,6 +4,9 @@ module.exports = function (req, res, next) {
     let nowTime = new Date(Date.now())
     nowTime.toISOString().split("T")[0]
     console.log(`%c ${nowTime}------------------------Start-`, "background: #222; color: #bada55")
+    console.log(`%c -------------------------`, "background: #222; color: #bada55")
+    console.log(req.url)
+    console.log(`%c -------------------------`, "background: #222; color: #bada55")
     console.log(req.body)
     console.log(`%c ${nowTime}--------------------------End-`, "background: #222; color: #bada55")
 
@@ -11,7 +14,7 @@ module.exports = function (req, res, next) {
     // This way it will make JSON Server that it's GET request
     // @note POST 會修改原始的 json 設定檔案內容，改成 GET 就可以避免這樣的情況了。
     req.method = "GET"
-    console.log(`🚩 req.method: POST 改成 GET`)
+    console.log(`🚩 ${req.url} req.method: POST 改成 GET`)
     req.query = req.body
   }
   // Continue to JSON Server router
